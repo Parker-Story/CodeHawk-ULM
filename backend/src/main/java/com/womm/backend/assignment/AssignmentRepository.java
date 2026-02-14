ca407aeb96a0aa6a4156609fcc512539503fc2be
JCarradine <jadecarradine21@gmail.com>
2026-02-14 00:59:01 -0600

    Branch Fix

--- backend/src/main/java/com/womm/backend/assignment/AssignmentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.womm.backend.assignment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AssignmentRepository extends JpaRepository<Assignment,Long> {
-}
