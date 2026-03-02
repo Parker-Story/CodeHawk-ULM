10c3d35d5d077d7aa19feff8d1757be5ef13e6fd
Parker Story <105521989+Parker-Story@users.noreply.github.com>
2026-03-01 18:58:23 -0600

    Delete Assignment Fix
    - Fixed delete assignment.

--- backend/src/main/java/com/womm/backend/repository/SubmissionRepository.java
+++ backend/src/main/java/com/womm/backend/repository/SubmissionRepository.java
@@ -5,9 +5,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import java.util.List;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface SubmissionRepository extends JpaRepository<Submission, SubmissionId> {
 
     @Query("SELECT s FROM Submission s WHERE s.assignment.id = :assignmentId")
     List<Submission> findByAssignmentId(@Param("assignmentId") Long assignmentId);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Submission s WHERE s.assignment.id = :assignmentId")
+    void deleteByAssignmentId(@Param("assignmentId") Long assignmentId);
 }
